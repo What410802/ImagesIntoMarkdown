19258f5ed6ec26681802d76bc9bcc606f02ab637
What410802 <what410802@126.com>
2024-06-07 21:27:20 +0800

    Add files via upload

---
+++ ImagesIntoMarkdown.py
@@ -0,0 +1,39 @@
+import os
+
+TempPath=r'D:\BiShaokang410802\_Temp'
+TempFile=os.path.join(TempPath,'_Temp.md')
+Help='''Command:
+name, iname, ctime, mtime; named, ... : Sort source Files according to file name, the integar in file name, created time, or modified time. 'd' signals descending. 
+Else is default.
+'''
+
+while(True):
+    Input=input('Input source path or "help" : ')
+    if Input=='help':
+        print(Help)
+        continue
+    SourcePath=Input
+    Command=input('Input command: ')
+    SourcePictures=os.listdir(SourcePath)
+    os.chdir(SourcePath)
+    if Command=='name':
+        sort(SourcePictures)
+    elif Command=='iname':
+        SourcePictures.sort(key=lambda File:int(File.split('.')[0]))
+    elif Command=='ctime':
+        SourcePictures.sort(key=lambda File:os.path.getctime(File))
+    elif Command=='mtime':
+        SourcePictures.sort(key=lambda File:os.path.getmtime(File))
+    elif Command=='named':
+        SourcePictures.sort(reverse)
+    elif Command=='inamed':
+        SourcePictures.sort(key=lambda File:int(File.split('.')[0]),reverse=True)
+    elif Command=='ctimed':
+        SourcePictures.sort(key=lambda File:os.path.getctime(File),reverse=True)
+    elif Command=='mtimed':
+        SourcePictures.sort(key=lambda File:os.path.getmtime(File),reverse=True)
+    with open(TempFile,'w') as TempFileH:
+        for SourcePicture in SourcePictures:
+            TempFileH.write(r'![](%s)'%(os.path.join(SourcePath,SourcePicture)))
+    os.startfile(TempFile)
+    break
